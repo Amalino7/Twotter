35aa5f2a2cefb7ccca23681e24fc6c351c2c8777
Amalino7 <alex.m.malinov@gmail.com>
2025-06-03 22:01:46 +0300

    Added basic server login. Keycloak secrets and setup need to be redone

--- backend/src/main/kotlin/elsys/amalino7/plugins/Security.kt
+++ backend/src/main/kotlin/elsys/amalino7/plugins/Security.kt
@@ -1,68 +1,68 @@
 package elsys.amalino7.plugins
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
 
 fun Application.configureSecurity() {
     authentication {
-        oauth("auth-oauth-google") {
+        oauth("keycloakOAuth") {
             urlProvider = { "http://localhost:8080/callback" }
             providerLookup = {
                 OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                    name = "keycloak",
+                    authorizeUrl = "http://localhost:7080/realms/kotlin/protocol/openid-connect/auth",
+                    accessTokenUrl = "http://localhost:7080/realms/kotlin/protocol/openid-connect/token",
                     requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
+                    clientId = "ktor",
+                    clientSecret = "iMtUPKeEYOrJzjghLewLOC02L6qkstxo", // TODO fix
+                    defaultScopes = listOf("openid", "profile", "email")
                 )
             }
             client = HttpClient(Apache)
         }
     }
-    // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
-    authentication {
-        jwt {
-            realm = jwtRealm
-            verifier(
-                JWT
-                    .require(Algorithm.HMAC256(jwtSecret))
-                    .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain)
-                    .build()
-            )
-            validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
-            }
-        }
-    }
+//    // Please read the jwt property from the config file if you are using EngineMain
+//    val jwtAudience = "jwt-audience"
+//    val jwtDomain = "https://jwt-provider-domain/"
+//    val jwtRealm = "ktor sample app"
+//    val jwtSecret = "secret"
+//    authentication {
+//        jwt {
+//            realm = jwtRealm
+//            verifier(
+//                JWT
+//                    .require(Algorithm.HMAC256(jwtSecret))
+//                    .withAudience(jwtAudience)
+//                    .withIssuer(jwtDomain)
+//                    .build()
+//            )
+//            validate { credential ->
+//                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+//            }
+//        }
+//    }
     routing {
-        authenticate("auth-oauth-google") {
+        authenticate("keycloakOAuth") {
             get("login") {
                 call.respondRedirect("/callback")
             }
 
             get("/callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/hello")
+                val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
+                if (principal != null) {
+                    val accessToken = principal.accessToken
+                    call.respondText("Access token: $accessToken")
+                } else {
+                    call.respond(HttpStatusCode.Unauthorized, "No token")
+                }
             }
         }
     }
 }
 
-class UserSession(accessToken: String)
+
