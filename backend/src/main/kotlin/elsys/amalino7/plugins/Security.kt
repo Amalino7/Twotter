cdde308e15cfff4187cb4bd8a288f3f83ccba7f4
Amalino7 <alex.m.malinov@gmail.com>
2025-06-12 18:13:46 +0300

    Added refresh token support for auth

--- backend/src/main/kotlin/elsys/amalino7/plugins/Security.kt
+++ backend/src/main/kotlin/elsys/amalino7/plugins/Security.kt
@@ -7,6 +7,8 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -14,11 +16,15 @@ import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.date.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 import java.net.URL
-import java.security.interfaces.RSAPublicKey
+
+val jwtDomain = "http://localhost:7080/realms/KtorAuth"
+val jwtRealm = "KtorAuth"
+val jwkProvider = UrlJwkProvider(URL("$jwtDomain/protocol/openid-connect/certs"))
 
 fun Application.configureSecurity() {
     authentication {
@@ -38,10 +44,6 @@ fun Application.configureSecurity() {
             }
             client = HttpClient(Apache)
         }
-
-        val jwtDomain = "http://localhost:7080/realms/KtorAuth"
-        val jwtRealm = "KtorAuth"
-        val jwkProvider = UrlJwkProvider(URL("$jwtDomain/protocol/openid-connect/certs"))
         jwt("auth-jwt") {
             realm = jwtRealm
             verifier(jwkProvider, jwtDomain) {
@@ -61,7 +63,7 @@ fun Application.configureSecurity() {
 
     routing {
         authenticate("keycloakOAuth") {
-            get("login") {
+            get("/login") {
                 // redirects
             }
 
@@ -82,20 +84,74 @@ fun Application.configureSecurity() {
                             header("Authorization", "Bearer $accessToken")
                         }.body()
 
+                    call.response.cookies.append(
+                        Cookie(
+                            name = "refresh_token",
+                            value = principal.refreshToken.toString(),
+//                            httpOnly = true,
+//                            secure = true,
+                            path = "/refresh",
+                            maxAge = 3600 * 24 * 7,
+                        )
+                    )
+
                     call.respondText("User info:\n${res} Access token: $accessToken\n Refresh token: ${principal.refreshToken}")
 
                 } else {
                     call.respond(HttpStatusCode.Unauthorized, "No token")
                 }
+
+
             }
         }
-    }
-}
+        post("/logout")
+        {
+            call.response.cookies.append(
+                "refresh_token", "", CookieEncoding.URI_ENCODING, 0, GMTDate.START
+            )
+            call.respond(HttpStatusCode.OK)
+        }
+        post("/refresh")
+        {
+            val refreshToken =
+                call.request.cookies["refresh_token"] ?: return@post call.respond(HttpStatusCode.Unauthorized)
 
-fun publicKeyFromKeycloak(): RSAPublicKey {
-    val jwkProvider = UrlJwkProvider(URL("http://localhost:7080/realms/KtorAuth"))
-    val key = jwkProvider.get("public_key")
-    return key.publicKey as RSAPublicKey
+            val httpClient = HttpClient(Apache) {}
+            val response: HttpResponse = httpClient.submitForm(
+                url = "$jwtDomain/protocol/openid-connect/token",
+                formParameters = Parameters.build {
+                    append("grant_type", "refresh_token")
+                    append("refresh_token", refreshToken)
+                    append("client_id", "ktor")
+                    append("client_secret", dotenv().get("CLIENT_SECRET"))
+                }
+            )
+
+            if (!response.status.isSuccess()) {
+                call.respond(HttpStatusCode.Unauthorized, "Refresh failed")
+                return@post
+            }
+
+            val tokenResponse = response.body<TokenResponse>()
+
+            call.response.cookies.append(
+                Cookie(
+                    name = "refresh_token",
+                    value = tokenResponse.refresh_token,
+                    httpOnly = true,
+                    secure = true,
+                    path = "/refresh",
+                    maxAge = 3600 * 24 * 7,
+                )
+            )
+
+            call.respond(
+                mapOf(
+                    "access_token" to tokenResponse.access_token
+                )
+            )
+        }
+    }
 }
 
 @OptIn(ExperimentalSerializationApi::class)
@@ -107,4 +163,13 @@ data class KeycloakUserInfo(
     val name: String
 )
 
+@Serializable
+data class TokenResponse(
+    val access_token: String,
+    val refresh_token: String,
+    val expires_in: Int,
+    val refresh_expires_in: Int,
+    val token_type: String,
+    val scope: String
+)
 
