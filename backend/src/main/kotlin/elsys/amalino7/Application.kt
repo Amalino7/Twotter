d742daa5a20bc09b36511c27f0ad76b077332e2e
Александър Малинов <alex.m.malinov@gmail.com>
2025-06-23 22:02:30 +0300

    Updated local env

--- backend/src/main/kotlin/elsys/amalino7/Application.kt
+++ backend/src/main/kotlin/elsys/amalino7/Application.kt
@@ -4,9 +4,16 @@ import connectToDatabase
 import elsys.amalino7.minio.minioClient
 import elsys.amalino7.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
